717e9ea1e3613a9271cff693cd1ec03ed66fde05
Frazer Grant <33299243+frzruk@users.noreply.github.com>
2018-11-19 22:38:32 +0000

    Update RFID-Write-Thinger/Thinger_Connection.ino
    This is no longer needed

---
+++ RFID-Write-Thinger/Thinger_Connection.ino
@@ -0,0 +1,13 @@
+//https://api.thinger.io/v2/users/frzruk/devices/esp8266_huzzah/led?authorization=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkZXYiOiJlc3A4MjY2X2h1enphaCIsImlhdCI6MTUzOTE3NTUyMSwianRpIjoiNWJiZGY0NjE5MjUxYWEwNGNjYzdlNWUyIiwicmVzIjpbImxlZCAiXSwidXNyIjoiZnJ6cnVrIn0.eSdGIzv3zxSodED9N5i-_eFTgoFxmrL6q7K2-uRdXHc
+//This sketch gets the board to display the Username, Time and Text of a tweet created by myself (@frzrg)
+//by hooking it into IFTTT and twitter commands as well as Thinger.io to handle the IoT part of it.
+
+
+
+
+
+
+
+void thingerSetup() {
+
+}
