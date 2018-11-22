79a03512799d9088e76dd84638715e1b7a0f19d6
Frazer Grant <33299243+frzruk@users.noreply.github.com>
2018-11-22 10:57:44 +0000

    Update RFID-WriteToNewTags.ino

--- RFID-WriteToNewTags/RFID-WriteToNewTags.ino
+++ RFID-WriteToNewTags/RFID-WriteToNewTags.ino
@@ -10,8 +10,6 @@
    SPI SCK    |  SCK      |   14
    -------------------------------------------------------------------
 */
-
-
 #include <SPI.h>
 #include <MFRC522.h>
 
@@ -19,6 +17,7 @@
 #define SS_PIN          4         // Configurable, see typical pin layout above
 
 MFRC522 rfid(SS_PIN, RST_PIN);  // Create MFRC522 instance
+MFRC522::MIFARE_Key key;
 
 void setup() {
   Serial.begin(9600);		// Initialize serial communications with the PC
@@ -29,6 +28,15 @@ void setup() {
   Serial.println(F("This code will check for the presence of a phone's NFC chip."));
   Serial.println(F("It then writes data to a new RFID tag (marble etc.) that's"));
   Serial.println(F("'taken' from social media accounts from the NFC on smartphones"));
+
+  // Prepare the key (used both as key A and key B)
+  // using FFFFFFFFFFFFh which is the default at chip delivery from the factory
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+
+  dumpByteArray(key.keyByte, MFRC522::MF_KEY_SIZE);
+  Serial.println();
 }
 
 void loop() {
@@ -44,10 +52,7 @@ void loop() {
 
   // This dumps the Unique ID of the NFC Chip in the phone
   Serial.print(F("Card UID:"));
-  for (byte i = 0; i < rfid.uid.size; i++) {
-    Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    Serial.print(rfid.uid.uidByte[i], HEX);
-  }
+  dumpByteArray(rfid.uid.uidByte, rfid.uid.size);
 
   // This dumps the PICC Type of the card
   Serial.print(F(" PICC type: "));
@@ -56,28 +61,8 @@ void loop() {
 
   
   // Start to write data to the new RFID tag in the code below
-  byte buffer[34];
-  byte block;
-  MFRC522::StatusCode status;
-  byte len;
 
-  // This block of code gets input from the Serial Monitor
-  Serial.setTimeout(20000L); // Wait for 20 seconds till timeout
-  Serial.println(F("Type in number of followers, end with #"));
-  len = Serial.readBytesUntil('#', (char *) buffer, 30); // This reads the data input into the Serial
-  for(byte i = len; i < 30; i++) buffer[i] = ' '; // Pad the rest with spaces
 
-  // Check the authentication of block 1
-  block = 1;
-  status = rfid.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(rfid.uid));
-  if (status != MFRC522::STATUS_OK) {
-    Serial.print(F("PCD_Authenticate() failed: "));
-    Serial.println(rfid.GetStatusCodeName(status));
-    return;
-  }
-  else Serial.println(F("PCD_Authenticate() success: "));
-
-  
   // Write information to variables and store on new RFID Marble
   //  Each of these need to be written to different blocks on RFID card
   //    Followers
@@ -87,4 +72,15 @@ void loop() {
   //    Avg. Colour Makeup of Photos
   //
   // Dump debug info about the card; PICC_HaltA() is automatically called
+
+ 
+ /*
+ * Helper routine to dump a byte array as hex values to Serial.
+ */
+void dumpByteArray(byte *buffer, byte bufferSize) {
+    for (byte i = 0; i < bufferSize; i++) {
+        Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+        Serial.print(buffer[i], HEX);
+    }
+}
 }
